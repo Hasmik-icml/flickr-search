0a56c0e0153e72db3be39bd95f73c2335b1fe3c5
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-06-16 21:56:44 +0400

    creating useState for  searchText
    creating useState for  searchText

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,30 @@
-import React from "react"
-import { useState } from "react"
+import React from "react";
+import { useState } from "react";
 
+function Search({onSearch}) {
+  const [text, setText] = useState("");
+ 
 
-function Search(){
-
-    return (
-        <div>
-            <form>
-                <input type="text" value={"text"} className="searchInput" placeholder="photos"></input>
-                <button>Search</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form 
+       onSubmit={(e) => {
+        e.preventDefault();
+        onSearch(text)
+      }}
+      >
+        <input
+          type="text"
+          value={text}
+          className="searchInput"
+          placeholder="photos"
+          onChange={(e)=>{
+            setText(e.target.value);
+          }}
+        ></input>
+        <button>Search</button>
+      </form>
+    </div>
+  );
 }
 export default Search;
