939f9c556ca95331ee8a2c5b851fc312fd0bf462
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-06-16 23:18:37 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import Search from "./components/Search";
 import Pics from "./components/Pics"
 import Baskets from "./components/Baskets";
 import FilteredPics from "./components/FilteredPics";
+import { useEffect, useState } from 'react';
+import { getPictures } from "./Services/api.js";
 
 function App() {
+const [searchText, setSearchText] = useState("");
+const [words, setWords] = useState("");
 
+async function showPics(){
+  const pics = await getPictures(words)
+  console.log(pics);
+}
+useEffect(()=>{
+  const searchingWords = searchText.split(" ").join("+");
+  setWords(searchingWords);
+},[searchText]);
 
+useEffect(()=>{
+ if (words.length > 0) showPics(words);
+},[words]);
   return (
     <>
-        <Search />
+        <Search onSearch={(text)=>{
+          setSearchText(text)
+          console.log(text);
+        }}/>
         <Pics />
         <Baskets />
         <FilteredPics />
